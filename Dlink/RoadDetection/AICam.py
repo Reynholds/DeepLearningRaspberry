0842b12fa0c13377f04530eb679ec1a38193612d
Reynholds REINETTE <reynholds.reinette@gmail.com>
2019-05-17 15:56:31 +0200

    Save by Tanguy !

---
+++ Dlink/RoadDetection/AICam.py
@@ -0,0 +1,157 @@
+import numpy as np
+import cv2 as cv
+import time
+import datetime
+import sys
+import logging
+
+#LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
+LOG_FILE = "SendToAzureLog"
+LOG_FORMAT ="%(asctime)s %(levelname)s %(message)s"
+logging.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, level=LOG_LEVEL)
+
+
+def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+if not sys.argv[1].strip() :
+    file_full_name ='/home/pi/Camera/sended/FTPClip20190511_144507.mp4'
+else:
+    file_full_name = sys.argv[1].strip()
+    
+# dotted rectangle https://stackoverflow.com/questions/26690932/opencv-rectangle-with-dotted-or-dashed-lines 
+
+logging.info(">AICam analyze {}\n".format(file_full_name))
+
+video_prefix = "FTPClip"
+#file_full_name = '/home/pi/Camera/sended/FTPClip20190511_144507.mp4'
+filename =  right(file_full_name, len(file_full_name) - file_full_name.rfind(video_prefix))
+year  = int(mid(filename, len(video_prefix), 4))
+month = int(mid(filename, len(video_prefix) +4, 2))
+day   = int(mid(filename, len(video_prefix) +4 +2, 2))
+hour  = int(mid(filename, len(video_prefix) +4 +2 +2 +1, 2))
+minute= int(mid(filename, len(video_prefix) +4 +2 +2 +1 +2, 2))
+second= int(mid(filename, len(video_prefix) +4 +2 +2 +1 +2 +2, 2))
+
+#print("{}-{}-{} {}:{}:{}".format(int(year),int(month), int(day), int(hour), int(minute), int(second)))
+#print("{}-{}-{} {}:{}:{}".format(year,month, day, hour, minute, second))
+
+
+# Load the Model 
+net = cv.dnn.readNet('/home/pi/RREI/Dlink/RoadDetection/person-vehicle-bike-detection-crossroad-0078-FP32.xml', '/home/pi/RREI/Dlink/RoadDetection/person-vehicle-bike-detection-crossroad-0078-FP32.bin')
+
+# Specify target device
+net.setPreferableTarget(cv.dnn.DNN_TARGET_MYRIAD)
+
+# Read an image
+cap = cv.VideoCapture(file_full_name)
+
+#image dimension
+width = 1280
+heigh = 640
+
+#Setting the frame jump
+time_length = cap.get(7)/cap.get(5)
+number_frames= cap.get(7)
+fps = cap.get(5)
+frame_jump = 20
+frame_seq = - frame_jump
+
+blue = (255,0,0)
+green = (0,255,0)
+red = (0,0,255)
+white = (255,255,255)
+black = (0,0,0)
+
+label = ("Pieton", "Vélo", "Voiture")
+
+ColorDetection =[blue, green, red, white, black]
+
+
+start_datetime = datetime.datetime.now()
+
+#Create output file
+txt_file = open(file_full_name.replace(".mp4",".json"), 'w+')
+logging.info("Create file {}\n".format(txt_file))
+print("Create file {}\n".format(txt_file))
+txt_file.write('{{\n  "record": {{\n    "filename" : {6},\n    "time_length" : {0},\n    "fps": {1},\n    "frame_jump" : {2},\n    "width" : {3},\n    "heigh" : {4},\n    "start_time" : "{5}",\n    "detections" :[\n'.format(time_length, fps, frame_jump, width, heigh, start_datetime, filename))
+
+
+while(True):
+    # Start time
+    start = time.time() 
+    
+    # Get a specific number of frame
+    frame_seq += frame_jump 
+    frame_no = (frame_seq /number_frames)
+    if frame_seq > number_frames :
+        break
+    cap.set(1,frame_seq)
+    
+    
+    # Capture specific frame
+    ret, frame = cap.read()
+    
+    print ("{4} : frame n°{0}/{1} -> frame_no = {2} at {3}".format(frame_seq,number_frames,frame_no,cap.get(0)/1000, datetime.datetime.now()) )     
+    # Prepare input blob and perform an inference
+    blob = cv.dnn.blobFromImage(frame, size=(1024,1024), ddepth=cv.CV_8U)
+    net.setInput(blob)
+    ## RREI Note : 1er lancement de cette instruction longue
+    out= net.forward()
+    
+    
+    # Draw detected faces on the frame
+    for detection in out.reshape(-1, 7):
+        confidence = float(detection[2])
+        xmin = int(detection[3] * frame.shape[1])
+        ymin = int(detection[4] * frame.shape[0])
+        xmax = int(detection[5] * frame.shape[1])
+        ymax = int(detection[6] * frame.shape[0])
+     
+     
+        if confidence > 0.5 :        
+          #Ecriture au format : ['frame':3, 'detection':'voiture', 'confiance':50, 'horodatage':'2019-05-14 13:45:59', position:[xmin=0,ymin=0,xmax=5,ymax=5]]
+           print("{{'frame':{0}, 'detection':'{1}' 'confiance':{2}, 'horodatage':'{3}', position:['xmin':{4},'ymin':{5},'xmax':{6},'ymax':{7}]}}\n".format(frame_no*number_frames, label[int(detection[1])], detection[2]*100, datetime.datetime.fromtimestamp(cap.get(0)/1000  + datetime.datetime(year, month, day, hour, minute, second, 0).timestamp()) , detection[3]* frame.shape[1],detection[4]* frame.shape[0],detection[5]* frame.shape[1],detection[6]* frame.shape[0]))
+           txt_file.write('        {{"frame":{0}, "detection":"{1}", "confiance":{2}, "horodatage":"{3}", "position":{{"xmin":{4},"ymin":{5},"xmax":{6},"ymax":{7}}}}},\n'.format(frame_no*number_frames, label[int(detection[1])], detection[2]*100, datetime.datetime.fromtimestamp(cap.get(0)/1000  + datetime.datetime(year, month, day, hour, minute, second, 0).timestamp()) , detection[3]* frame.shape[1],detection[4]* frame.shape[0],detection[5]* frame.shape[1],detection[6]* frame.shape[0]))
+           #cv.rectangle(frame, (xmin, ymin), (xmax, ymax), ColorDetection[int(detection[1])], thickness=5)
+        #elif confidence > 0.35 :
+           #cv.rectangle(frame, (xmin, ymin), (xmax, ymax), ColorDetection[int(detection[1])], thickness=2)
+        #elif confidence > 0.15 :
+           #cv.rectangle(frame,(xmin, ymin) , (xmax, ymax), ColorDetection[int(detection[1])], thickness=1)
+
+
+    # resize the image
+    #frame = cv.resize(frame,(width,heigh))
+
+
+    #end time
+    end = time.time()
+
+    # Time elapsed
+    seconds = end - start
+    fps = 1 / seconds 
+
+    #cv.putText(frame, "FPS : {0:.2f} ".format(fps), (30,30), cv.FONT_HERSHEY_COMPLEX, 1, (0,0,0), thickness = 1)
+    
+
+    # Display the resulting frame
+    #cv.imshow('AI Cam', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+end_datetime = datetime.datetime.now()
+duration = divmod((end_datetime- start_datetime).total_seconds(), 60)
+txt_file.write('        {{"EndOfDetections":true}}],\n    "end_time":"{}",\n    "duration":"{}"\n }} \n}}'.format(datetime.datetime.now(), duration))   
+txt_file.close()
+cv.destroyAllWindows()
+
+
---
+++ Dlink/RoadDetection/SendToAzure.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+import os
+import glob
+import re
+import time
+import datetime
+import uuid, sys
+from azure.storage.blob import BlockBlobService, PublicAccess
+import logging
+
+#LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
+LOG_FILE = "SendToAzureLog"
+LOG_FORMAT ="%(asctime)s %(levelname)s %(message)s"
+logging.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, level=LOG_LEVEL)
+
+
+video_prefix = "FTPClip"
+
+def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+def run_sample(full_path_to_file):
+    try:
+        # Create the BlockBlockService that is used to call the Blob service for the storage account
+        block_blob_service = BlockBlobService(account_name='pavic', account_key='lPdOTarDD4n3xuwz5EHMNOK3q/SNpnZ/1FKYKHNJSL4V4ZPt1LlvtjTZlpCfnghYFYQi20nTpBx6GVRPKfghoQ==')
+
+        # Create a container called 'quickstartblobs'.
+        container_name ='aicamvideoclips'
+        block_blob_service.create_container(container_name)
+        #block_blob_service.cre
+
+        # Set the permission so the blobs are public.
+        block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
+
+        # Create a file in Documents to test the upload and download.
+        local_path=os.path.abspath(os.path.curdir)
+        local_file_name = right(full_path_to_file, len(full_path_to_file) - full_path_to_file.rfind(video_prefix))# input("Enter file name to upload : ")
+        #full_path_to_file =os.path.join(local_path, local_file_name)
+
+        # Write text to the file.
+        #file = open(full_path_to_file,  'w')
+        #file.write("Hello, World!")
+        #file.close()
+		
+        year  = mid(local_file_name, len(video_prefix), 4)
+        month = mid(local_file_name, len(video_prefix) +4, 2)
+        day   = mid(local_file_name, len(video_prefix) +4 +2, 2)
+        hour  = mid(local_file_name, len(video_prefix) +4 +2 +2 +1, 2)
+
+        print("Temp file = " + full_path_to_file)
+        print("\nUploading to Blob storage as blob " + local_file_name)
+        logging.info("Uploading {0}, to container {1}\\{2}\\{3}\\{4}".format(full_path_to_file, year, month, day, hour))
+		
+
+        # Upload the created file, use local_file_name for the blob name
+        block_blob_service.create_blob_from_path(container_name,  year+"\\"+month+"\\"+day+"\\"+hour+"\\"+local_file_name, full_path_to_file)
+        logging.info("Uploaded {0}, to container {1}\\{2}\\{3}\\{4}".format(full_path_to_file, year, month, day, hour))
+        print("\nUploaded to Blob storage as blob " + local_file_name)      
+        #Move uploaded file to sended directory
+        try:
+                os.mkdir(path+"//sended")
+        except Exception as e:
+                print(e)
+        os.rename(full_path_to_file, full_path_to_file.replace(video_prefix,"sended/"+video_prefix))
+        print("{0} Move to sended\n".format(local_file_name))
+        logging.info("{} move to /sended\n".format(local_file_name))
+
+    except Exception as e:
+        print(e)
+
+
+path = os.getcwd()
+
+
+files = []
+
+# r=root, d=directories, f = files
+#for r, d, f in os.walk(path):
+#    for file in f:
+#        if '.mp4' in file:
+#            files.append(os.path.join(r, file))
+
+files_in_dir = os.listdir(path)
+for file in files_in_dir :
+    if '.mp4' in file:
+            files.append(os.path.join(path, file))
+
+            
+print ("\n{0}\t{1} files to send\n".format(str(datetime.datetime.now()),len(files)))
+logging.info(("\n{0}\t{1} files to send\n".format(str(datetime.datetime.now()),len(files))))
+
+for f in files:
+    print(f)
+    filename = right(f, len(f) - f.rfind(video_prefix))
+    print(filename)
+    logging.info(("\n{0}\t starting AI inference on \n".format(str(datetime.datetime.now()),f)))
+    logging.info('/usr/bin/python3 /home/pi/Camera/AICam.py "{}"'.format(f))
+    
+    os.system('LD_LIBRARY_PATH="/home/pi/openvino/inference_engine_vpu_arm/opencv/lib:/opt/intel/opencl:/home/pi/openvino/inference_engine_vpu_arm/deployment_tools/inference_engine/external/hddl/lib:/home/pi/openvino/inference_engine_vpu_arm/deployment_tools/inference_engine/external/gna/lib:/home/pi/openvino/inference_engine_vpu_arm/deployment_tools/inference_engine/external/mkltiny_lnx/lib:/home/pi/openvino/inference_engine_vpu_arm/deployment_tools/inference_engine/external/omp/lib:/home/pi/openvino/inference_engine_vpu_arm/deployment_tools/inference_engine/lib/raspbian_9/armv7l:" PYTHONPATH="/home/pi/openvino/inference_engine_vpu_arm/python/python3.5:/home/pi/openvino/inference_engine_vpu_arm/python/python3.5/armv7l:/home/pi/openvino/inference_engine_vpu_arm/deployment_tools/model_optimizer:" python3 AICam.py "{}"'.format(f)) 
+    
+    run_sample(f)
+    run_sample(f.replace(".mp4",".json"))
+
